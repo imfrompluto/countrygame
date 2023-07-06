bd87489969d4adfc8be72304eb0af6d7ac997945
polina <87384505+imfrompluto@users.noreply.github.com>
2023-07-06 18:53:52 +0100

    Add files via upload

--- game.py
+++ game.py
@@ -8,26 +8,49 @@ window = tk.Tk()
 randomnumber = random.randint(0,len(flags.flags))
 scorenumber = 0
 score = tk.Label(text='score: ' + str(scorenumber))
-score.config(font =(None,50))
+score.config(font =(None, 15))
 flag = tk.Label(text=flags.flags[randomnumber])
 flag.config(font =(None,150))
 countries = [
     flags.countries[random.randint(0,len(flags.flags))],
     flags.countries[random.randint(0,len(flags.flags))],
     flags.countries[random.randint(0,len(flags.flags))],
-    
     ]
+
 countries.insert(random.randrange(4),flags.countries[randomnumber])
+def checkflag(country):
+    global scorenumber
+    global randomnumber
+    global countries
+    global button
+    global button2
+    global button3
+    global button4
 
-def checkflag(n):
-    print("aaa")
-    n = n + 1
-    score.config(text='score: ' + str(n))
+    print(country, flags.countries[randomnumber])
+    if country == flags.countries[randomnumber]: 
+        scorenumber = scorenumber + 1
+        score.config(text='score: ' + str(scorenumber))
+        randomnumber = random.randint(0,len(flags.flags))
+        flag.config(text=flags.flags[randomnumber])
+        countries = [
+            flags.countries[random.randint(0,len(flags.flags))],
+            flags.countries[random.randint(0,len(flags.flags))],
+            flags.countries[random.randint(0,len(flags.flags))],
+        ]
+        countries.insert(random.randrange(4),flags.countries[randomnumber])
+        button.config(text=countries[0])
+        button2.config(text=countries[1])
+        button3.config(text=countries[2])
+        button4.config(text=countries[3])
 
-button = tk.Button(window, text = countries[0], fg='black', bg="black", command=partial(checkflag,scorenumber))
-button2 = tk.Button(window, text = countries[1], fg="black", bg="black")
-button3 = tk.Button(window, text = countries[2], fg="black", bg="black")
-button4 = tk.Button(window, text = countries[3], fg="black", bg="black")
+
+
+
+button = tk.Button(window, text = countries[0], fg='black', bg="black", command=partial(checkflag, countries[0]))
+button2 = tk.Button(window, text = countries[1], fg="black", bg="black", command=partial(checkflag, countries[1]))
+button3 = tk.Button(window, text = countries[2], fg="black", bg="black", command=partial(checkflag, countries[2]))
+button4 = tk.Button(window, text = countries[3], fg="black", bg="black", command=partial(checkflag, countries[3]))
 
 
 
